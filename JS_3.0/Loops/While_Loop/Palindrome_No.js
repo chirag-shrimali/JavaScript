5de923aeca89562777a8cd4f1df580e87abaad37
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-01-29 17:44:44 +0530

    Palindrome No

---
+++ JS_3.0/Loops/While_Loop/Palindrome_No.js
@@ -0,0 +1,25 @@
+// write the java script program to check that user entering the number is palindrome or not
+
+no = parseInt(prompt("\nEnter the No : "));
+
+temp = no;
+
+rev = 0;
+
+while(no > 0)
+{
+    rem = no % 10;
+
+    rev = (rev * 10) + rem;
+
+    no = Math.floor(no / 10);
+}
+
+if(rev == temp)
+{
+    console.log(temp,"is a Palindrome No...");
+}
+else
+{
+    console.log(temp,"is not a Palindrome No...");
+}
