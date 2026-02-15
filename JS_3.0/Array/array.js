db12e29752d8ede7181a1b16dbd726836d958a56
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-15 21:41:47 +0530

    Array Programs

--- JS_3.0/Array/array.js
+++ JS_3.0/Array/array.js
@@ -13,23 +13,37 @@
 
 // find the Maximum and Minimum value in the array list -----------------------------------------------------
 
-//         0    1    2    3    4
-let arr = [90 , 50 , 63 , 78 , 45];
+// //         0    1    2    3    4
+// let arr = [90 , 50 , 63 , 78 , 45];
 
-let max = min = arr[0];
+// let max = min = arr[0];
+
+// for(let i = 0;i < arr.length;i++)
+// {
+//     if(arr[i] > max)
+//     {
+//         max = arr[i];
+//     }
+//     if(arr[i] < min)
+//     {
+//         min = arr[i]
+//     }
+// }
+
+// console.log("\nThe Maximum Value is :",max);
+
+// console.log("\nThe Minimum Value is :",min);
+
+// Sum of the Array Lists ---------------------------------------------------------------
+
+//         0    1    2    3    4     5
+let arr = [90 , 50 , 63 , 78 , 45 , 89];
+
+let sum = 0;
 
 for(let i = 0;i < arr.length;i++)
 {
-    if(arr[i] > max)
-    {
-        max = arr[i];
-    }
-    if(arr[i] < min)
-    {
-        min = arr[i]
-    }
+    sum += arr[i]; // sum = sum + arr[i];
 }
 
-console.log("\nThe Maximum Value is :",max);
-
-console.log("\nThe Minimum Value is :",min);
+console.log("\nThe Sum is : ",sum);
