63babb4f0b80572a9d8afca9cc155236a70f4f77
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-15 22:31:10 +0530

    Maps Filter FlatMap Tasks

--- JS_3.0/Array/Maps/map.js
+++ JS_3.0/Array/Maps/map.js
@@ -1,7 +1,7 @@
 //             0           1         2          3          4          5
-let data = ['Chirag' , 'Suresh' , "Rakesh" , "Mukesh" , 'Mahesh' , 'Ramesh'];
+// let data = ['Chirag' , 'Suresh' , "Rakesh" , "Mukesh" , 'Mahesh' , 'Ramesh'];
 
-let upper = [];
+// let upper = [];
 
 // for(let i = 0;i < data.length;i++)
 // {
@@ -34,8 +34,159 @@ let upper = [];
 
 // console.log(lower);
 
-let arr = [5 , 8 , 10 , 20 , 7]
+// let arr = [5 , 8 , 10 , 20 , 7]
 
-check = arr.map((e)=> e >= 8)
+// check = arr.map((e)=> e >= 8)
 
-console.log(check);
+// console.log(check);
+
+// Task - 1 Palindrome Return only in the other array using map
+
+// let data = ["Chirag" , 'naman' , "Naman"];
+
+// let pal = data.map((e)=>
+// {
+//     return e.split(0 , data.length);
+// });
+
+// console.log(pal);
+
+// // Upper Case -------------------------------------------------
+
+// //             0           1         2          3          4          5
+// let data = ['Chirag' , 'Suresh' , "Rakesh" , "Mukesh" , 'Mahesh' , 'Ramesh'];
+
+// console.log(data);
+
+// let result = data.map((e)=>e.toUpperCase());
+
+// console.log(result);
+
+//             0           1         2          3          4          5
+// let data = ['Chirag' , 'Suresh' , "Rakesh" , "Mukesh" , 'Mahesh' , 'Ramesh'];
+
+// console.log(data);
+
+// let result = data.flatMap((e)=>e.toUpperCase());
+
+// console.log(result);
+
+// Lower Case -------------------------------------------------
+
+// //             0           1         2          3          4          5
+// let data = ['Chirag' , 'Suresh' , "Rakesh" , "Mukesh" , 'Mahesh' , 'Ramesh'];
+
+// console.log(data);
+
+// let result = data.map((e)=>e.toLowerCase());
+
+// console.log(result);
+
+// //             0           1         2          3          4          5
+// let data = ['Chirag' , 'Suresh' , "Rakesh" , "Mukesh" , 'Mahesh' , 'Ramesh'];
+
+// console.log(data);
+
+// let result = data.flatMap((e)=>e.toLowerCase());
+
+// console.log(result);
+
+// ------------------------------------------------------------------------------------------------------
+
+// let arr = [5 , 10 , 20];
+
+// console.log(arr);
+
+// let result = arr.filter((e)=>e);
+
+// console.log(result);
+
+// let result = arr.map((e)=>e);
+
+// console.log(result);
+
+// let result = arr.flatMap((e)=>e);
+
+// console.log(result);
+
+// let result = arr.map((e)=>{
+//     return e > 8;
+// });
+
+// console.log(result);
+
+// let result = arr.flatMap((e)=>{
+//     return e > 8;
+// });
+
+// console.log(result);
+
+// let result = arr.filter((e)=>{
+//     return e > 8;
+// });
+
+// console.log(result);
+
+// ----------------------------------------------------------------------------------------------------------
+
+// let arr = ['a' , 'b' , 'c'];
+
+// console.log(arr);
+
+// // let result = arr.flatMap((e , i)=>i + " - " + e);
+
+// let result = arr.map((e , i)=>i + " - " + e);
+
+// console.log(result);
+
+// ----------------------------------------------------------------------------------------------------------
+
+// IMPORTANT :- for reverse the string string.split('').reverse().join('');
+
+// // //             0          1         2
+// let arr = ['chirag' , 'ramesh' , 'naman'];
+
+// console.log(arr);
+
+// let result = arr.map((e)=>{
+//     return e == e.split('').reverse().join('')
+// });
+
+// console.log(result);
+
+// // //         0          1         2          3
+// let arr = ['madam' , 'chirag' , 'ramesh' , 'naman'];
+
+// console.log(arr);
+
+// let result = arr.flatMap((e)=>{
+//     return e == e.split('').reverse().join('')
+// });
+
+// console.log(result);
+
+// // //         0          1         2          3
+let arr = ['madam' , 'chirag' , 'ramesh' , 'naman'];
+
+console.log(arr);
+
+let result = arr.filter((e)=>{
+    return e == e.split('').reverse().join('')
+});
+
+console.log(result);
+
+// ---------------------------------------------------------------------------------------------------------
+
+//             0          1         2
+// let arr = ['chirag' , 'ramesh' , 'naman'];
+
+// let arr1 = [];
+
+// for(let i = 0;i < arr.length ; i++)
+// {
+//     if(arr[i].split('').reverse().join('') == arr[i])
+//     {
+//         console.log(arr1.push(i));
+//     }
+// }
