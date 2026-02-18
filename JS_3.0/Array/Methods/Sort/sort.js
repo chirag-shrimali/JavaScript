d8abed1f3c2e0f333b5be6b864c2060e103f183c
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-18 23:21:34 +0530

    Sort Programs

---
+++ JS_3.0/Array/Methods/Sort/sort.js
@@ -0,0 +1,11 @@
+// in the array their are having the methods like sort...
+
+//            0           1         2           3         4
+
+let data = ["Rahul" , "Chirag" , "Chintan" , "Ramesh" , "Suresh"];
+
+console.log(data);
+
+data.sort()
+
+console.log(data);
