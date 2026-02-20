cfb0a74752e4fc066229d6974f1a27212df71793
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-20 16:50:34 +0530

    Sort Programs

--- JS_3.0/Array/Methods/Sort/sort.js
+++ JS_3.0/Array/Methods/Sort/sort.js
@@ -1,11 +1,140 @@
 // in the array their are having the methods like sort...
 
+// ----------------------------------------------------------------------------------------------------
+
+//              0      1       2      3
+
+// let salary = [10000 , 2000 , 3000 , 40000];
+
+// let result = salary.map((a)=>a + 1000);
+
+// let result = salary.flatMap((a)=>a + 1000);
+
+// let result = salary.map((a)=>{
+//     return a + 1000;
+// });
+
+// let result = salary.flatMap((a)=>{
+//     return a + 1000;
+// });
+
+// let result = salary.reduce((a , b)=>
+// {
+//     a.push(b + 1000)
+
+//     return a;
+// } , []);
+
+// console.log(salary);
+
+// console.log(result);
+
+// ----------------------------------------------------------------------------------------------------
+
 //            0           1         2           3         4
 
-let data = ["Rahul" , "Chirag" , "Chintan" , "Ramesh" , "Suresh"];
+// let data = ["Rahul" , "Chirag" , "Chintan" , "Ramesh" , "Suresh"];
 
-console.log(data);
+//          0    1    2    3     4
 
-data.sort()
+// let data = [78 , 63 , 12 , 45 , 96]
 
-console.log(data);
+// console.log(data);
+
+// data.sort()
+
+// console.log(data);
+
+// -------------------------------------------------------------------------------------------------
+
+// let data = ["Mukesh" , "Rahul" , "Sumit" , "Ketan" , "Chirag" , "Om" , "Jay" , "Suresh"];
+
+// console.log(data);
+
+// ascending order
+
+// let result = data.sort((a , b)=> a.length - b.length);
+
+// let result = data.sort((a , b)=> {
+//     return a.length - b.length
+// });
+
+// decending order
+
+// let result = data.sort((a , b)=> b.length - a.length);
+
+// let result = data.sort((a , b)=>{
+//     return b.length - a.length
+// });
+
+// console.log(result);
+
+// ---------------------------------------------------------------------------------------------------------
+
+// let data = [1000 , 50000 , 4000 , 2500 , 1200];
+
+// console.log(data);
+
+// ascending order
+
+// let result = data.sort((a , b)=>a - b);
+
+// let result = data.sort((a , b)=>{
+//     return a - b
+// });
+
+// descending order
+
+// let result = data.sort((a , b)=>b - a);
+
+// let result = data.sort((a , b)=>{
+//     return b - a
+// });
+
+// console.log(result);
+
+// -------------------------------------------------------------------------------------------------
+
+let emp = [
+    {
+        name : "Chirag" , age : 19 , city : "ahmedabad"
+    } ,
+
+    {
+        name : "Rahul" , age : 25 , city : "delhi"
+    }
+];
+
+// console.log(emp);
+
+// ascending order
+
+// let result = emp.sort((a ,b)=> a.age - b.age);
+
+// let result = emp.sort((a ,b)=>{
+//     return a.age - b.age}
+// );
+
+// descending order
+
+// let result = emp.sort((a , b)=>b.age - a.age);
+
+// let result = emp.sort((a , b)=>{
+//     return b.age - a.age
+// });
+
+// --------------------------
+
+// ascending order wise name
+
+// let result = emp.sort((a , b)=>{
+//     return a.name.length - b.name.length;
+// });
+
+// desending order wise name
+
+let result = emp.sort((a , b)=>{
+    return b.name.length - a.name.length;
+});
+
+console.log(result);
