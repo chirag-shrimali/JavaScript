64b25c7a7ca09982d86596b4e43029f4204ad5f8
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-25 19:56:55 +0530

    Function Programs

--- JS_3.0/Functions/function_type.js
+++ JS_3.0/Functions/function_type.js
@@ -6,60 +6,86 @@ In the Function, basically there are having 4 types of functions :-
 4. WA and WR
 */
 
-// 1. WOA and WOR
+// // 1. WOA and WOR
 
-function add()
+// function add()
+// {
+//     let a = 100 , b = 50 , c;
+
+//     c = a + b;
+
+//     console.log("\nThe Addition of two numbers are :",c);
+// }
+
+// add()
+
+// // ------------------------------------------------------------------
+
+// // 2. WA and WOR
+
+// function sub(a , b)
+// {
+//     let c;
+
+//     c = a - b;
+
+//     console.log("\nThe Subtraction of two numbers are :",c);
+// }
+
+// sub(100 , 50)
+
+// // --------------------------------------------------------------------
+
+// // 3. WOA and WR
+
+// function mul()
+// {
+//     let a = 100 , b = 50 , c;
+
+//     c = a * b;
+
+//     return c;
+// }
+
+// console.log("\nThe Multiplication of Two numbers are :",mul());
+
+// // ----------------------------------------------------------------------
+
+// // 4. WA and WR
+
+// function div(a , b)
+// {
+//     let c;
+
+//     c = a / b;
+
+//     return c;
+// }
+
+// console.log("\nThe Division of Two numbers are :",div(100 , 50));
+
+// ---------------------------------------------------------------------------
+
+// function fullName(fname , lname)
+// {
+//     console.log("\nThe First Name is",fname,"and the Last Name is",lname,"and the FullName is :",fname + " " + lname);
+// }
+
+// fullName("Chirag" , "Shrimali")
+
+// Find the Digit ----------------------------------------------------------------
+
+function Digit(no)
 {
-    let a = 100 , b = 50 , c;
+    let count = 0;
 
-    c = a + b;
+    while(no > 0)
+    {
+        no = Math.floor(no / 10);
 
-    console.log("\nThe Addition of two numbers are :",c);
+        count += 1; // count = count + 1 , count++
+    }
+        console.log("\nThe Total Number of Digits are :",count);
 }
 
-add()
-
-// ------------------------------------------------------------------
-
-// 2. WA and WOR
-
-function sub(a , b)
-{
-    let c;
-
-    c = a - b;
-
-    console.log("\nThe Subtraction of two numbers are :",c);
-}
-
-sub(100 , 50)
-
-// --------------------------------------------------------------------
-
-// 3. WOA and WR
-
-function mul()
-{
-    let a = 100 , b = 50 , c;
-
-    c = a * b;
-
-    return c;
-}
-
-console.log("\nThe Multiplication of Two numbers are :",mul());
-
-// ----------------------------------------------------------------------
-
-// 4. WA and WR
-
-function div(a , b)
-{
-    let c;
-
-    c = a / b;
-
-    return c;
-}
-
-console.log("\nThe Division of Two numbers are :",div(100 , 50));
+Digit(1234567);
