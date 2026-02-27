fdf13a8c1dc3bbf00cce7ec56d9815a08f43f2da
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-02-27 17:38:41 +0530

    Arrow Function Programs

---
+++ JS_3.0/Arrow Function/arrow.js
@@ -0,0 +1,40 @@
+// normal function...
+
+// function test()
+// {
+//     console.log("Hello World!!");
+// }
+
+// test()
+
+// using Arrow Function -----------------------------
+
+// const test = ()=>{
+//     console.log("Hello World!!");
+// }
+
+// test()
+
+const add = (a , b)=>a + b;
+
+const sub = (a , b)=>a - b;
+
+const mul = (a , b)=>a * b;
+
+const div = (a , b)=>a / b;
+
+let result = add(100 , 20);
+
+let result1 = sub(100 , 20);
+
+let result2 = mul(100 , 20);
+
+let result3 = div(100 , 20);
+
+console.log("The Addition is :",result);
+
+console.log("The Subtraction is :",result1);
+
+console.log("The Multiplication is :",result2);
+
+console.log("The Division is :",result3);
